ef4d143f750fbb44c45ba1d7c08901810045ad6d
lucas <lucas@DESKTOP-1ETI925>
2023-02-16 14:35:34 +0100

    Added a sum function and class Calculate.cs

---
+++ Calculate.cs
@@ -0,0 +1,10 @@
+namespace GetCheckpoint1
+{
+    internal static class Calculate
+    {
+        public static void SumOfThis(this int a, int b)
+        {
+            Console.WriteLine($"The sum of {a} + {b} = {a+b}.");
+        }
+    }
+}
