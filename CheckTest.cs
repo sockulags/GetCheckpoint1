f0b1015bbba70617397221f3c663fe836d8df87d
lucas <lucas@DESKTOP-1ETI925>
2023-02-16 14:18:45 +0100

    Task 1,2 completed

---
+++ CheckTest.cs
@@ -0,0 +1,10 @@
+namespace GetCheckpoint1
+{
+    internal static class CheckTest
+    {
+       public static void TestMessage(this string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GetCheckpoint1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the Git checkpoint task 1.1");
+            CheckTest.TestMessage("Task 2 checked");
+
+        }
+    }
+}
